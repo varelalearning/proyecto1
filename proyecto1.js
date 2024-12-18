db439fc41edac1f03236cff4b600f0b8da46559f
Pablo Rodriguez <pabloerodriguez1991@gmail.com>
2024-12-18 00:30:31 -0300

    agregamos footer y seccion

--- proyecto1.js
+++ proyecto1.js
@@ -1,1 +1,18 @@
-// probando subir cosas
+// Selección de elementos del DOM
+const heroForm = document.querySelector(".email-input");
+const ctaForm = document.querySelector(".cta__form");
+const navLinks = document.querySelectorAll(".header-link");
+
+// Validación de email
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+  
+  function handleFormSubmit(event) {
+    const input = event.target.querySelector('input[type="email"]');
+    if (!validateEmail(input.value)) {
+      event.preventDefault();
+      alert('Please enter a valid email address.');
+    }
+  }
